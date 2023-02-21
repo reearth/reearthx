129c2f5183991ef9f9617e0379f6c5f2e157c8cb
awakot <kawamura.estyle@gmail.com>
2023-02-21 19:15:04 +0900

    fix(account): add tests

--- account/accountusecase/accountinteractor/user_test.go
+++ account/accountusecase/accountinteractor/user_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/reearth/reearthx/account/accountdomain"
 	"github.com/reearth/reearthx/account/accountdomain/user"
 	"github.com/reearth/reearthx/account/accountinfrastructure/accountmemory"
+	"github.com/reearth/reearthx/account/accountusecase/accountgateway"
+	"github.com/reearth/reearthx/mailer"
 	"github.com/reearth/reearthx/rerror"
 
 	"github.com/stretchr/testify/assert"
@@ -96,6 +98,173 @@ func TestUser_VerifyUser(t *testing.T) {
 			} else {
 				assert.Nil(t, u)
 			}
+			assert.Equal(t, tt.wantError, err)
+		})
+	}
+}
+
+func TestUser_StartPasswordReset(t *testing.T) {
+	user.DefaultPasswordEncoder = &user.NoopPasswordEncoder{}
+	uid := accountdomain.NewUserID()
+	tid := accountdomain.NewWorkspaceID()
+	r := accountmemory.New()
+
+	m := mailer.NewMock()
+	g := &accountgateway.Container{Mailer: m}
+	uc := NewUser(r, g, "", "")
+	tests := []struct {
+		name             string
+		createUserBefore *user.User
+		email            string
+		wantMailSubject  string
+		wantMailTo       []accountgateway.Contact
+		wantError        error
+	}{
+		{
+			name: "ok",
+			createUserBefore: user.New().
+				ID(uid).
+				Workspace(tid).
+				Email("aaa@bbb.com").
+				Name("NAME").
+				Auths([]user.Auth{
+					{
+						Provider: user.ProviderReearth,
+						Sub:      "reearth|" + uid.String(),
+					},
+				}).
+				MustBuild(),
+			email:           "aaa@bbb.com",
+			wantMailSubject: "Password reset",
+			wantMailTo: []accountgateway.Contact{
+				{
+					Email: "aaa@bbb.com",
+					Name:  "NAME",
+				},
+			},
+			wantError: nil,
+		},
+		{
+			name:      "not found",
+			email:     "ccc@bbb.com",
+			wantError: rerror.ErrNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			if tt.createUserBefore != nil {
+				assert.NoError(t, r.User.Save(ctx, tt.createUserBefore))
+			}
+			err := uc.StartPasswordReset(ctx, tt.email)
+
+			if err != nil {
+				assert.Equal(t, tt.wantError, err)
+			} else {
+				user, err := r.User.FindByEmail(ctx, tt.email)
+				assert.NoError(t, err)
+				assert.NotNil(t, user.PasswordReset())
+			}
+
+			mails := m.Mails()
+			if tt.wantMailSubject != "" {
+				assert.Equal(t, 1, len(mails))
+				assert.Equal(t, tt.wantMailSubject, mails[0].Subject)
+				assert.Equal(t, tt.wantMailTo, mails[0].To)
+			}
+		})
+	}
+}
+
+func TestUser_PasswordReset(t *testing.T) {
+	user.DefaultPasswordEncoder = &user.NoopPasswordEncoder{}
+	uid := accountdomain.NewUserID()
+	tid := accountdomain.NewWorkspaceID()
+	r := accountmemory.New()
+	uc := NewUser(r, nil, "", "")
+	pr := user.NewPasswordReset()
+	expired := time.Now().Add(24 * time.Hour)
+	tests := []struct {
+		name             string
+		password         string
+		token            string
+		createUserBefore *user.User
+		wantError        error
+	}{
+		{
+			name:     "ok",
+			password: "PAss00!!",
+			token:    pr.Token,
+			createUserBefore: user.New().
+				ID(uid).
+				Workspace(tid).
+				Name("NAME").
+				Email("aaa@bbb.com").
+				PasswordPlainText("PAss00!!").
+				Verification(user.VerificationFrom("code", expired, false)).
+				PasswordReset(pr).
+				Auths([]user.Auth{
+					{
+						Provider: user.ProviderReearth,
+						Sub:      "reearth|" + uid.String(),
+					},
+				}).
+				MustBuild(),
+			wantError: nil,
+		},
+		{
+			name:     "invalid password",
+			password: "pass",
+			token:    pr.Token,
+			createUserBefore: user.New().
+				ID(uid).
+				Workspace(tid).
+				Name("NAME").
+				Email("aaa@bbb.com").
+				PasswordPlainText("PAss00!!").
+				Verification(user.VerificationFrom("code", expired, false)).
+				PasswordReset(pr).
+				Auths([]user.Auth{
+					{
+						Provider: user.ProviderReearth,
+						Sub:      "reearth|" + uid.String(),
+					},
+				}).
+				MustBuild(),
+			wantError: user.ErrPasswordLength,
+		},
+		{
+			name:     "not found",
+			password: "PAss00!!",
+			token:    pr.Token,
+			createUserBefore: user.New().
+				ID(uid).
+				Workspace(tid).
+				Name("NAME").
+				Email("aaa@bbb.com").
+				PasswordPlainText("PAss00!!").
+				Verification(user.VerificationFrom("code", expired, false)).
+				Auths([]user.Auth{
+					{
+						Provider: user.ProviderReearth,
+						Sub:      "reearth|" + uid.String(),
+					},
+				}).
+				MustBuild(),
+			wantError: rerror.ErrNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			if tt.createUserBefore != nil {
+				assert.NoError(t, r.User.Save(ctx, tt.createUserBefore))
+			}
+			err := uc.PasswordReset(ctx, tt.password, tt.token)
 			assert.Equal(t, tt.wantError, err)
 		})
 	}
