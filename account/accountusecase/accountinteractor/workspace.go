2708e7635e592cdbfa1d809af06936f99d12915d
nourbalaha <nourbalaha0@gmail.com>
2024-10-23 15:47:28 +0900

    unify logic

--- account/accountusecase/accountinteractor/workspace.go
+++ account/accountusecase/accountinteractor/workspace.go
@@ -187,44 +187,8 @@ func (i *Workspace) AddIntegrationMember(ctx context.Context, wId workspace.ID, 
 	})
 }
 
-func (i *Workspace) RemoveUserMember(ctx context.Context, id workspace.ID, u workspace.UserID, operator *accountusecase.Operator) (_ *workspace.Workspace, err error) {
-	if operator.User == nil {
-		return nil, accountinterfaces.ErrInvalidOperator
-	}
-
-	return Run1(ctx, operator, i.repos, Usecase().Transaction(), func(ctx context.Context) (*workspace.Workspace, error) {
-		ws, err := i.repos.Workspace.FindByID(ctx, id)
-		if err != nil {
-			return nil, err
-		}
-
-		if ws.IsPersonal() {
-			return nil, workspace.ErrCannotModifyPersonalWorkspace
-		}
-
-		isOwner := ws.Members().UserRole(*operator.User) == workspace.RoleOwner
-		isSelfLeave := *operator.User == u
-		if !isOwner && !isSelfLeave {
-			return nil, accountinterfaces.ErrOperationDenied
-		}
-
-		if isSelfLeave && ws.Members().IsOnlyOwner(u) {
-			return nil, accountinterfaces.ErrOwnerCannotLeaveTheWorkspace
-		}
-
-		err = ws.Members().Leave(u)
-		if err != nil {
-			return nil, err
-		}
-
-		err = i.repos.Workspace.Save(ctx, ws)
-		if err != nil {
-			return nil, err
-		}
-
-		i.applyDefaultPolicy(ws, operator)
-		return ws, nil
-	})
+func (i *Workspace) RemoveUserMember(ctx context.Context, id workspace.ID, u workspace.UserID, operator *accountusecase.Operator) (*workspace.Workspace, error) {
+	return i.RemoveMultipleUserMembers(ctx, id, workspace.UserIDList{u}, operator)
 }
 
 func (i *Workspace) RemoveMultipleUserMembers(ctx context.Context, id workspace.ID, userIds workspace.UserIDList, operator *accountusecase.Operator) (_ *workspace.Workspace, err error) {
