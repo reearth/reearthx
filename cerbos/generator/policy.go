490130bc5877157dcb62963dfb39cd3e2c2e3c33
akiyatomohiro <thairwars@gmail.com>
2024-10-30 14:50:56 +0900

    add policy file generation

---
+++ cerbos/generator/policy.go
@@ -0,0 +1,76 @@
+package generator
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type ResourceDefinition interface {
+	GetResource() string
+	GetActions() []ActionDefinition
+}
+
+type ActionDefinition interface {
+	GetAction() string
+	GetRoles() []string
+}
+
+type CerbosPolicy struct {
+	APIVersion     string         `yaml:"apiVersion"`
+	ResourcePolicy ResourcePolicy `yaml:"resourcePolicy"`
+}
+
+type ResourcePolicy struct {
+	Version  string `yaml:"version"`
+	Resource string `yaml:"resource"`
+	Rules    []Rule `yaml:"rules"`
+}
+
+type Rule struct {
+	Actions []string `yaml:"actions"`
+	Effect  string   `yaml:"effect"`
+	Roles   []string `yaml:"roles"`
+}
+
+func GeneratePolicyFiles(resources []ResourceDefinition, outputDir string) error {
+	for _, resource := range resources {
+		policy := CerbosPolicy{
+			APIVersion: "api.cerbos.dev/v1",
+			ResourcePolicy: ResourcePolicy{
+				Version:  "default",
+				Resource: resource.GetResource(),
+				Rules:    make([]Rule, 0, len(resource.GetActions())),
+			},
+		}
+
+		for _, action := range resource.GetActions() {
+			rule := Rule{
+				Actions: []string{action.GetAction()},
+				Effect:  "EFFECT_ALLOW",
+				Roles:   action.GetRoles(),
+			}
+			policy.ResourcePolicy.Rules = append(policy.ResourcePolicy.Rules, rule)
+		}
+
+		filename := strings.ReplaceAll(resource.GetResource(), ":", "_")
+		outputPath := filepath.Join(outputDir, fmt.Sprintf("%s.yaml", filename))
+
+		if err := os.MkdirAll(outputDir, 0755); err != nil {
+			return fmt.Errorf("failed to create directory: %w", err)
+		}
+
+		data, err := yaml.Marshal(policy)
+		if err != nil {
+			return fmt.Errorf("failed to marshal policy: %w", err)
+		}
+
+		if err := os.WriteFile(outputPath, data, 0644); err != nil {
+			return fmt.Errorf("failed to write file: %w", err)
+		}
+	}
+	return nil
+}
