c7840c02325295fe96a6519db7e420d050df69eb
akiyatomohiro <thairwars@gmail.com>
2024-11-06 22:32:32 +0900

    fix CheckPermission

--- cerbos/client/permission_checker.go
+++ cerbos/client/permission_checker.go
@@ -7,12 +7,6 @@ import (
 	"github.com/reearth/reearthx/appx"
 )
 
-type ContextKey string
-
-const (
-	contextAuthInfo ContextKey = "authinfo"
-)
-
 type PermissionChecker struct {
 	Service      string
 	DashboardURL string
@@ -25,8 +19,7 @@ func NewPermissionChecker(service string, dashboardURL string) *PermissionChecke
 	}
 }
 
-func (p *PermissionChecker) CheckPermission(ctx context.Context, resource string, action string) (bool, error) {
-	authInfo := getAuthInfo(ctx)
+func (p *PermissionChecker) CheckPermission(ctx context.Context, authInfo *appx.AuthInfo, resource string, action string) (bool, error) {
 	if authInfo == nil {
 		return false, fmt.Errorf("auth info not found")
 	}
@@ -39,13 +32,4 @@ func (p *PermissionChecker) CheckPermission(ctx context.Context, resource string
 
 	client := NewClient(p.DashboardURL)
 	return client.CheckPermission(ctx, authInfo, input)
-}
-
-func getAuthInfo(ctx context.Context) *appx.AuthInfo {
-	if v := ctx.Value(contextAuthInfo); v != nil {
-		if v2, ok := v.(appx.AuthInfo); ok {
-			return &v2
-		}
-	}
-	return nil
 }
