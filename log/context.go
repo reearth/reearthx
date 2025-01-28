5675c40c2eb1ab96263cf93153d6e22b2619b568
rot1024 <aayhrot@gmail.com>
2025-01-28 18:17:50 +0900

    fix(log): fix caller skip

--- log/context.go
+++ log/context.go
@@ -20,9 +20,9 @@ func GetLoggerFromContext(ctx context.Context) *Logger {
 
 func GetLoggerFromContextOrDefault(ctx context.Context) *Logger {
 	if logger := GetLoggerFromContext(ctx); logger != nil {
-		return logger.AddCallerSkip(1)
+		return logger
 	}
-	return globalLogger.AddCallerSkip(1)
+	return globalLogger
 }
 
 func UpdateContext(ctx context.Context, f func(logger *Logger) *Logger) context.Context {
@@ -30,65 +30,72 @@ func UpdateContext(ctx context.Context, f func(logger *Logger) *Logger) context.
 }
 
 func Tracefc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Debugf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Debugf(format, args...)
 }
 
 func Debugfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Debugf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Debugf(format, args...)
 }
 
 func Infofc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Infof(format, args...)
+	getLoggerFromContextOrDefault(ctx).Infof(format, args...)
 }
 
 func Printfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Infof(format, args...)
+	getLoggerFromContextOrDefault(ctx).Infof(format, args...)
 }
 
 func Warnfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Warnf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Warnf(format, args...)
 }
 
 func Errorfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Errorf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Errorf(format, args...)
 }
 
 func Fatalfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Fatalf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Fatalf(format, args...)
 }
 
 func Panicfc(ctx context.Context, format string, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Panicf(format, args...)
+	getLoggerFromContextOrDefault(ctx).Panicf(format, args...)
 }
 
 func Tracec(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Debug(args...)
+	getLoggerFromContextOrDefault(ctx).Debug(args...)
 }
 
 func Debugc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Debug(args...)
+	getLoggerFromContextOrDefault(ctx).Debug(args...)
 }
 
 func Infoc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Info(args...)
+	getLoggerFromContextOrDefault(ctx).Info(args...)
 }
 
 func Printc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Info(args...)
+	getLoggerFromContextOrDefault(ctx).Info(args...)
 }
 
 func Warnc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Warn(args...)
+	getLoggerFromContextOrDefault(ctx).Warn(args...)
 }
 
 func Errorc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Error(args...)
+	getLoggerFromContextOrDefault(ctx).Error(args...)
 }
 
 func Fatalc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Fatal(args...)
+	getLoggerFromContextOrDefault(ctx).Fatal(args...)
 }
 
 func Panicc(ctx context.Context, args ...any) {
-	GetLoggerFromContextOrDefault(ctx).Panic(args...)
+	getLoggerFromContextOrDefault(ctx).Panic(args...)
+}
+
+func getLoggerFromContextOrDefault(ctx context.Context) *Logger {
+	if logger := GetLoggerFromContext(ctx); logger != nil {
+		return logger.AddCallerSkip(1)
+	}
+	return globalLogger
 }
