d41c14e5ebeaffd4a3028c52be38e0bfba213f3c
akiyatomohiro <thairwars@gmail.com>
2025-02-20 20:21:50 +0800

    add role and permittable accountrepo

---
+++ account/accountusecase/accountrepo/permittable.go
@@ -0,0 +1,16 @@
+package accountrepo
+
+import (
+	"context"
+
+	"github.com/reearth/reearthx/account/accountdomain"
+	"github.com/reearth/reearthx/account/accountdomain/permittable"
+	"github.com/reearth/reearthx/account/accountdomain/user"
+)
+
+type Permittable interface {
+	FindByUserID(context.Context, user.ID) (*permittable.Permittable, error)
+	FindByUserIDs(context.Context, user.IDList) (permittable.List, error)
+	FindByRoleID(context.Context, accountdomain.RoleID) (permittable.List, error)
+	Save(context.Context, permittable.Permittable) error
+}
