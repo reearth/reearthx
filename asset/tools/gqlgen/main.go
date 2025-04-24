0556efb6d01aaabcc71f46c38fec1d80e41c4938
xy <xyzmhx@gmail.com>
2025-04-25 06:44:36 +0900

    fix(gqlgen): suppress error on directory creation in ensureDir function

--- asset/tools/gqlgen/main.go
+++ asset/tools/gqlgen/main.go
@@ -68,6 +68,6 @@ func findAssetDir() (string, error) {
 
 func ensureDir(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.MkdirAll(dir, 0755)
+		_ = os.MkdirAll(dir, 0755)
 	}
 }
