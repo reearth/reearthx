7f3b9c32b561cc616d6c0609981aa51204009eba
xy <xyzmhx@gmail.com>
2025-06-02 14:03:00 +0900

    feat(repo): add Event, Group, Request, and WorkspaceSettings interfaces for improved structure

---
+++ asset/usecase/repo/event.go
@@ -0,0 +1,14 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/reearth/reearth-cms/server/pkg/event"
+	"github.com/reearth/reearth-cms/server/pkg/id"
+)
+
+type Event interface {
+	FindByID(context.Context, id.EventID) (*event.Event[any], error)
+	Save(context.Context, *event.Event[any]) error
+	SaveAll(context.Context, event.List) error
+}
