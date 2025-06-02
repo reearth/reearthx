ac09f8fd9fb82b6c168080dd074d10ce6631e821
xy <xyzmhx@gmail.com>
2025-06-02 13:59:34 +0900

    feat(thread): implement Thread and Comment structures with builder pattern and associated methods

---
+++ asset/domain/thread/comment.go
@@ -0,0 +1,52 @@
+package thread
+
+import (
+	"github.com/reearth/reearthx/asset/domain/operator"
+	"time"
+)
+
+type Comment struct {
+	id      CommentID
+	author  operator.Operator
+	content string
+}
+
+func NewComment(id CommentID, author operator.Operator, content string) *Comment {
+	return &Comment{
+		id:      id,
+		author:  author,
+		content: content,
+	}
+}
+
+func (c *Comment) ID() CommentID {
+	return c.id
+}
+
+func (c *Comment) Author() operator.Operator {
+	return c.author
+}
+
+func (c *Comment) Content() string {
+	return c.content
+}
+
+func (c *Comment) CreatedAt() time.Time {
+	return c.id.Timestamp()
+}
+
+func (c *Comment) SetContent(content string) {
+	c.content = content
+}
+
+func (c *Comment) Clone() *Comment {
+	if c == nil {
+		return nil
+	}
+
+	return &Comment{
+		id:      c.id,
+		author:  c.author,
+		content: c.content,
+	}
+}
